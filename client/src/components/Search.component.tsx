6f13fa2c235f074e71a4cfa6a31694e1c267fbe5
Mizan <mizan@Mizans-Mac-mini.local>
2022-07-10 16:03:38 +0600

    propertyAndPrice-dropdown

--- client/src/components/Search.component.tsx
+++ client/src/components/Search.component.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// import components
 import CountryDropdown from "./CountryDropdown.component";
-import PriceRangeDropdown from "./PriceRangeDropdown.component";
 import PropertyDropdown from "./PropertyDropdown.component";
+import PriceRangeDropdown from "./PriceRangeDropdown.component";
+
+// import context
+import { HouseContext } from "./HouseContext.component";
+// import icon
 import { RiSearch2Line } from "react-icons/ri";
+
 const Search = () => {
+  const { handleClick }: any = useContext(HouseContext);
   return (
-    <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative  lg:-top-4 lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg ">
+    <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg">
       <CountryDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
-      <button className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg  ">
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+        className="bg-violet-700 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-lg"
+      >
         <RiSearch2Line />
       </button>
     </div>
